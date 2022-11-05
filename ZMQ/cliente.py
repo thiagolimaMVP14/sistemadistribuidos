82440b4853195fc15a5be27154204f724f10bc2d
ThiagoLima_30 <92695665+thiagolimaMVP14@users.noreply.github.com>
2022-11-04 21:00:43 -0300

    Add files via upload

---
+++ ZMQ/cliente.py
@@ -0,0 +1,24 @@
+import zmq
+
+context = zmq.Context()
+
+#  Socket to talk to server
+print("Connecting to hello world server…")
+socket = context.socket(zmq.REQ)
+socket.connect("tcp://localhost:2424")
+
+#  Do 10 requests, waiting each time for a response
+while True:
+    message = input("Insira operacao('soma' 'sub' 'mult')> ")
+    socket.send_string(message)
+    message = socket.recv_string()
+    print("[ %s ]" % message)
+    message = input("valor> ")
+    socket.send_string(message)
+    message = socket.recv_string()
+    print("[ %s ]" % message)
+    message = input("valor> ")
+    socket.send_string(message)
+
+    message = socket.recv_string()
+    soma = int(message)
